334a56aaf6993c236f63a022a0919c5d623516d7
RhysMcguire <rhys.mcguire@hotmail.com>
2022-11-17 12:13:53 +0000

    removed unused imports

--- src/main/java/com/gorup22/App.java
+++ src/main/java/com/gorup22/App.java
@@ -1,8 +1,6 @@
 package com.gorup22;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
