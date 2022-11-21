72a424046edc2dc42ad457a877e98a2974703499
cleggacus <cleggacus@gmail.com>
2022-11-21 13:25:02 +0000

    canvas pane

--- src/main/java/com/gorup22/GamePane.java
+++ src/main/java/com/gorup22/GamePane.java
@@ -1,13 +1,16 @@
 package com.gorup22;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 enum State {
-    Start, Paused
+    Start, Playing, Paused
 }
 
 public class GamePane extends StackPane {
+    private GraphicsContext graphicsContext;
     private MenuPane startMenu;
     private MenuPane pauseMenu = new MenuPane();
     private Pane canvasPane = new Pane();
@@ -18,23 +21,44 @@ public class GamePane extends StackPane {
 
         this.setUpStartMenu();
         this.setUpPauseMenu();
+        this.setUpCanvasPane();
 
         this.setState(State.Start);
     }
 
 
     public void setState(State state) {
-        System.out.println(state);
         switch (state) {
             case Start:
                 this.startMenu.setVisible(true);
                 this.pauseMenu.setVisible(false);
+                this.canvasPane.setVisible(false);
+                break;
+            case Playing:
+                this.startMenu.setVisible(false);
+                this.pauseMenu.setVisible(false);
+                this.canvasPane.setVisible(true);
                 break;
             case Paused:
                 this.startMenu.setVisible(false);
                 this.pauseMenu.setVisible(true);
+                this.canvasPane.setVisible(true);
                 break;
         }
+    }
+
+    public GraphicsContext getGraphicsContext() {
+        return graphicsContext;
+    }
+
+    private void setUpCanvasPane() {
+        Canvas canvas = new Canvas();
+        this.canvasPane = new Pane(canvas);
+
+        canvas.widthProperty().bind(this.widthProperty());
+        canvas.heightProperty().bind(this.heightProperty());
+
+        this.graphicsContext = canvas.getGraphicsContext2D();
     }
 
     private void setUpStartMenu() {
