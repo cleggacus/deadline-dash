88f999275fa273ff4bf6c841a607b9cd3313a31c
cleggacus <cleggacus@gmail.com>
2022-11-23 19:16:02 +0000

    key down implimented

--- src/main/java/com/gorup22/KeyboardManager.java
+++ src/main/java/com/gorup22/KeyboardManager.java
@@ -1,7 +1,15 @@
 package com.gorup22;
 
+import java.util.HashSet;
+
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
 public class KeyboardManager {
     private static KeyboardManager instance;
+    
+    private Scene scene;
+    private HashSet<KeyCode> keysDown = new HashSet<>();
 
     private KeyboardManager() {}
 
@@ -10,4 +18,33 @@ public class KeyboardManager {
             instance = new KeyboardManager();
         return instance;
     } 
+
+    public void setScene(Scene scene) {
+        keysDown.clear();
+
+        if (this.scene != null) {
+            this.scene.setOnKeyPressed(null);
+            this.scene.setOnKeyReleased(null);
+        }
+
+        this.scene = scene;
+
+        scene.setOnKeyPressed((e -> {
+            keysDown.add(e.getCode());
+        }));
+
+        scene.setOnKeyReleased((e -> {
+            keysDown.remove(e.getCode());
+        }));
+    }
+
+    public boolean isKeyDown(KeyCode keyCode) {
+        return keysDown.contains(keyCode);
+    }
+
+    public KeyCode[] getKeysDown() {
+        KeyCode[] keyCodes = new KeyCode[this.keysDown.size()];
+        this.keysDown.toArray(keyCodes);
+        return keyCodes;
+    }
 }
