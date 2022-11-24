f8f7c5e4235a35ffed733f3633382722e8b70156
cleggacus <cleggacus@gmail.com>
2022-11-24 21:26:43 +0000

    Renderer javadocs

--- src/main/java/com/group22/Renderer.java
+++ src/main/java/com/group22/Renderer.java
@@ -5,6 +5,15 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+/**
+ * 
+ * The class {@code Renderer} is an abstraction for drawing tiles to the Graphics Context.
+ * 
+ * Renderer allows for images to be drawn at positions in a grid and be resized accoding to the canvas size.
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
 public class Renderer {
     private static final double DEFAULT_WIDTH = 15;
     private static final double DEFAULT_HEIGHT = 10;
@@ -17,24 +26,56 @@ public class Renderer {
 
     private GraphicsContext graphicsContext;
 
+    /**
+     * Creates a renderer.
+     * 
+     * @param graphicsContext
+     */
     public Renderer(GraphicsContext graphicsContext) {
         this.graphicsContext = graphicsContext;
         this.graphicsContext.setImageSmoothing(false);
     }
 
+    
+    /** 
+     * Sets the width and height of the grid used for drawing images.
+     * 
+     * @param width 
+     * @param height
+     */
     public void setViewSize(int width, int height) {
         this.viewWidth = width;
         this.viewHeight = height;
     }
 
+    
+    /** 
+     * Gets the height to the grid to render images.
+     * 
+     * @return int
+     */
     public int getViewHeight() {
         return (int)viewHeight;
     }
 
+    
+    /** 
+     * Gets the width to the grid to render images.
+     * 
+     * @return int
+     */
     public int getViewWidth() {
         return (int)viewWidth;
     }
 
+    
+    /** 
+     * Draws an image at the position (x, y) in the grid.
+     * 
+     * @param image
+     * @param x
+     * @param y
+     */
     public void drawImage(Image image, double x, double y) {
         if(x < -1 || y < -1 || x > viewWidth || y > viewHeight)
             return;
@@ -51,6 +92,10 @@ public class Renderer {
         );
     }
 
+    /**
+     * To be run at the start of every frame.
+     * Clears the canvas and updates the Renderer details if canvas has been resized
+     */
     public void newFrame() {
         Canvas canvas = graphicsContext.getCanvas();
 
