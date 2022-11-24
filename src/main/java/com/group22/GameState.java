d466c6f7e6eaf657bba8d9ca389222cf7cc4d75c
cleggacus <cleggacus@gmail.com>
2022-11-24 21:09:26 +0000

    GameState javadocs

--- src/main/java/com/group22/GameState.java
+++ src/main/java/com/group22/GameState.java
@@ -1,5 +1,12 @@
 package com.group22;
 
+/**
+ * 
+ * The enum {@code GameState} represents the game state of {@code Engine}.
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
 public enum GameState {
     Start, Playing, Paused
 }
