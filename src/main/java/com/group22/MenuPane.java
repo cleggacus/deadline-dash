d740dacb5b38ee38802c926fe517e8ed639411f8
cleggacus <cleggacus@gmail.com>
2022-11-24 21:20:08 +0000

    MenuPane javadocs

--- src/main/java/com/group22/MenuPane.java
+++ src/main/java/com/group22/MenuPane.java
@@ -6,9 +6,21 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+/**
+ * 
+ * The class {@code MenuPane} is a scene layout element which extends GridPane.
+ * 
+ * MenuPane is used to abstract the menu into its own pane.
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
 public class MenuPane extends GridPane {
     private int count = 0;
 
+    /**
+     * Creates a MenuPane.
+     */
     public MenuPane() {
         this.getStyleClass().add("menu-outer");
         this.setAlignment(Pos.CENTER);
@@ -16,6 +28,12 @@ public class MenuPane extends GridPane {
         this.setVgap(10);
     }
 
+    
+    /** 
+     * Adds a button element to the menues list.
+     * @param name the text displayed on the button.
+     * @param onClick determins what happens when a button is clicked
+     */
     public void addItem(String name, Runnable onClick) {
         Button button = new Button(name);
 
