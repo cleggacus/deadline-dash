2e246b830acee48eaa45ef1d1cceb418d8e180c2
cleggacus <cleggacus@gmail.com>
2022-11-25 01:25:19 +0000

    test object

--- src/main/java/com/group22/TestObject.java
+++ src/main/java/com/group22/TestObject.java
@@ -2,33 +2,42 @@ package com.group22;
 
 import javafx.scene.input.KeyCode;
 
-public class TestObject extends LandMover {
+public class TestObject extends Entity {
     public TestObject() {
         super();
 
         this.setSprite("character/0.png");
 
         this.spriteOffsetY = -0.3;
-        this.moveEvery = 0.1;
+        this.moveEvery = 0.075;
     }
 
     @Override
     protected void updateMovement() {
-        if(Game.getInstance().getKeyState(KeyCode.W)) {
+        if(Game.getInstance().getKeyState(KeyCode.W) && validMove(0, -1)) {
             this.setSprite("character/3.png");
-            move(0, -1);
-        } else if(Game.getInstance().getKeyState(KeyCode.S)) {
+            this.y --;
+        } else if(Game.getInstance().getKeyState(KeyCode.S) && validMove(0, 1)) {
             this.setSprite("character/0.png");
-            move(0, 1);
+            this.y ++;
         }
 
-        if(Game.getInstance().getKeyState(KeyCode.A)) {
+        if(Game.getInstance().getKeyState(KeyCode.A) && validMove(-1, 0)) {
             this.setSprite("character/1.png");
-            move(-1, 0);
-        } else if(Game.getInstance().getKeyState(KeyCode.D)) {
+            this.x --;
+        } else if(Game.getInstance().getKeyState(KeyCode.D) && validMove(1, 0)) {
             this.setSprite("character/2.png");
-            move(1, 0);
+            this.x ++;
         }
+    }
+
+    private boolean validMove(double x, double y) {
+        double newX = this.x + x;
+        double newY = this.y + y;
+
+        return 
+            newX >= 0 && newX < Game.getInstance().getViewWidth() &&
+            newY >= 0 && newY < Game.getInstance().getViewHeight();
     }
 
     @Override
