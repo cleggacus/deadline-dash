f2e711994c9a100266195de72c83f630fd472b41
lmeekings <117082417+lmeekings@users.noreply.github.com>
2022-11-28 17:05:14 +0000

    Changing constructors

--- src/main/java/com/group22/Lever.java
+++ src/main/java/com/group22/Lever.java
@@ -4,9 +4,10 @@ public class Lever extends PickUp {
     private String leverColour;
     private Boolean leverIsOpen = false;
 
-    public Lever(int leverX, int leverY, Sprite leverSprite, String leverColour){
-        super(leverX, leverY, leverSprite);
+    public Lever(int leverX, int leverY, String leverColour){
+        super(leverX, leverY);
         setLeverColour(leverColour);
+        this.getSprite().setImage("lever.png");
     }
 
     private void setLeverColour(String leverColour){
@@ -25,6 +26,11 @@ public class Lever extends PickUp {
         return this.leverIsOpen;
     }
 
+    /**
+     * Searches through a list of gates via the list of entities, opening gates
+     * of the same colour as the lever.
+     * @param landMover the object activating the pickup
+     */
     @Override
     public void activatePickUpEffect(LandMover landMover) {
         for (Entity gate : Game.getInstance().getEntities(Gate.class)){
@@ -32,6 +38,7 @@ public class Lever extends PickUp {
                  ((Gate) gate).setGateIsOpen(true);
             }
         }
+        Game.getInstance().removeEntity(this);
     }
 
     @Override
