94b50523fbaedd5fe405160767b79d48a927d0ed
GowerHoldings <78835505+GowerHoldings@users.noreply.github.com>
2022-11-28 20:58:29 +0000

    Extended Level and LevelLoader classes

--- src/main/java/com/group22/Level.java
+++ src/main/java/com/group22/Level.java
@@ -1,36 +1,110 @@
 package com.group22;
+
+import java.util.List;
+
 public class Level {
-    private boolean locked;
+    private int timeToComplete;
     private String title;
     private int height;
     private int width;
-    private String[][] tiles;
-    private String[][] entities;
+    private Tile[][] tiles;
+    private List<? super Entity> entities;
     private String[][] scores;
-    /*
-    TBC.....
-
-    private Tile[] tiles;
-    private Entity[] entities;
-    private Score[] scores;*/
-
-    public Level(String title, boolean locked, int height, int width,  String[][] tiles, String[][] entities, String[][] scores){
+    
+    
+    public Level(String title, int timeToComplete, int height, int width,  String[][] tiles, String[][] entities, String[][] scores){
         this.title = title;
-        this.locked = locked;
+        this.timeToComplete = timeToComplete;
         this.height = height;
         this.width = width;
-        this.tiles = tiles;
-        this.entities = entities;
+        this.tiles = parseTiles(tiles);
+        this.entities = parseEntities(entities);
         this.scores = scores;
     }
 
-    /*
-    TBC.....
-    
-    public Tile[] getTiles(){
-        return tiles;
+    private Tile[][] parseTiles(String[][] tiles){
+        Tile[][] levelTiles = new Tile[width][height];
+
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                levelTiles[x][y] = new Tile(x, y, tiles[x][y]);
+            }
+        }
+        return levelTiles;
     }
 
+    private List<Entity> parseEntities(String[][] entities){
+        for(int i=0; i<entities.length; i++){
+            switch(entities[i][0]){
+                case("player"):
+                    /* Awaiting Player class implementation
+
+                    this.entities.add(new Player(
+                        entities[i][1],
+                        entities[i][2]
+                        ));
+                    */
+                    break;
+                case("door"):
+                    /* Awaiting Door class implementation
+
+                    this.entities.add(new Door(
+                        Integer.parseInt(entities[i][1]),
+                        Integer.parseInt(entities[i][2]),
+                        new Image("door_closed.png")));
+                    */
+                    break;
+                case("clock"):
+                    /* Awaiting Clock class implementation
+
+                    this.entities.add(new Clock(
+                        Integer.parseInt(entities[i][1]),
+                        Integer.parseInt(entities[i][2]),
+                        Integer.parseInt(entities[i][3])
+                        ));
+                    */
+                    break;
+                case("bomb"):
+                    /* Awaiting Bomb class implementation
+
+                    this.entities.add(new Bomb(
+                        Integer.parseInt(entities[i][1]),
+                        Integer.parseInt(entities[i][2]),
+                        Integer.parseInt(entities[i][3])
+                        ));
+                    */
+                    break;
+                case("followingthief"):
+                    /* Awaiting FollowingThief class implementation
+
+                    this.entities.add(new FollowingThief(
+                        Integer.parseInt(entities[i][1]),
+                        Integer.parseInt(entities[i][2]),
+                        new Sprite("character/following_thief.png"),
+                        entities[i][3]
+                        ));
+                    */
+                    break;
+                case("loot"):
+                    /* Awaiting Loot class implementation
+
+                    this.entities.add(new Loot(
+                        Integer.parseInt(entities[i][1]),
+                        Integer.parseInt(entities[i][2]),
+                        new Sprite("item/{entities[i][0]}.png"),
+                        Integer.parseInt(entities[i][3])
+                        ));
+                    */
+                    break;
+
+            }
+        }
+
+        return null;
+    }
+
+    /*
+    TBC.....
     public Entity[] getEntities(){
         return entities;
     }
@@ -43,19 +117,19 @@ public class Level {
         return title;
     }
 
-    public boolean getIsLocked(){
-        return locked;
+    public int getTimeToComplete(){
+        return timeToComplete;
     }
 
     public String[][] getHighscores(){
         return scores;
     }
 
-    public String[][] getTiles(){
+    public Tile[][] getTiles(){
         return tiles;
     }
 
-    public String[][] getEntities(){
+    public List<? super Entity> getEntities(){
         return entities;
     }
 
