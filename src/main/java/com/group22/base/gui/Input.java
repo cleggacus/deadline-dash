4b3b44724a9c970b6653acf7c9bd663681a369a2
cleggacus <cleggacus@gmail.com>
2022-12-01 02:31:07 +0000

    Input Gui element

---
+++ src/main/java/com/group22/base/gui/Input.java
@@ -0,0 +1,34 @@
+package com.group22.base.gui;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+
+public class Input extends BorderPane {
+    public Input(String placeholder, String buttonText, InputEvent onEnter) {
+        Button button = new Button(buttonText);
+
+        TextField textField= new TextField();
+
+        textField.setPromptText(placeholder);
+
+        textField.setOnKeyPressed(e -> {
+            if(e.getCode() == KeyCode.ENTER) {
+                onEnter.run(textField.getText());
+            }
+        });
+
+        button.setOnAction(e -> onEnter.run(textField.getText()));
+
+        this.setCenter(textField);
+        this.setRight(button);
+
+        this.getStyleClass().add("input");
+    }
+
+    public interface InputEvent {
+        void run(String s);
+    }
+}
+
