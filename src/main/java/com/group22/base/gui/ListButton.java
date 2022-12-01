0af38aba0962d4745fbdf1bdb0c29c92057930a8
cleggacus <cleggacus@gmail.com>
2022-12-01 02:31:51 +0000

    new list button gui element

---
+++ src/main/java/com/group22/base/gui/ListButton.java
@@ -0,0 +1,31 @@
+package com.group22.base.gui;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class ListButton extends BorderPane {
+    public ListButton(String text, OnClickEvent onClick, String removeText, RemoveButtonEvent onClickRemove) {
+        Button b1 = new Button();
+        b1.setText(text);
+        b1.setOnAction(e -> onClick.run());
+
+        Button b2 = new Button();
+        b2.setText(removeText);
+        b2.setOnAction(e -> onClickRemove.run(this));
+
+        b2.getStyleClass().add("remove");
+
+        this.setCenter(b1);
+        this.setRight(b2);
+
+        this.getStyleClass().add("list-button");
+    }
+
+    public interface RemoveButtonEvent {
+        void run(ListButton listButton);
+    }
+
+    public interface OnClickEvent {
+        void run();
+    }
+}
