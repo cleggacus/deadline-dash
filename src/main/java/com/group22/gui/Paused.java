1029510a40acedc258ad796f625e39ef9ae50e7a
cleggacus <cleggacus@gmail.com>
2022-12-01 14:07:39 +0000

    game over button removed

--- src/main/java/com/group22/gui/Paused.java
+++ src/main/java/com/group22/gui/Paused.java
@@ -7,7 +7,6 @@ public class Paused extends MenuPane {
     public Paused(GamePane gamePane) {
         this.addH1("PAUSED");
         this.addButton("RESUME", () -> gamePane.setState(GameState.Playing));
-        this.addButton("GAMEOVER", () -> gamePane.setState(GameState.GameOver));
         this.addButton("EXIT", () -> gamePane.setState(GameState.Start));
 
         this.getStyleClass().add("pause-menu");
