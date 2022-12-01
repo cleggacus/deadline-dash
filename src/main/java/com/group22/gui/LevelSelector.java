190e61ffcdb22745fa908795bd18e7e3f04b1719
cleggacus <cleggacus@gmail.com>
2022-12-01 19:42:56 +0000

    Image Level Selector

--- src/main/java/com/group22/gui/LevelSelector.java
+++ src/main/java/com/group22/gui/LevelSelector.java
@@ -4,30 +4,39 @@ import java.util.List;
 
 import com.group22.Game;
 import com.group22.GameState;
+import com.group22.gui.base.ImageList;
 import com.group22.gui.base.MenuPane;
 
 public class LevelSelector extends MenuPane {
-    private MenuPane levelMenu;
+    private ImageList imageList;
 
     public LevelSelector(GamePane gamePane) {
-        this.levelMenu = new MenuPane();
+        this.imageList = new ImageList();
 
         this.addH1("LEVELS");
-        this.add(this.levelMenu.getAsScrollPane());
+
+        this.add(imageList);
+
         this.addButton("GO BACK", () -> gamePane.setState(GameState.Start));
+
+        this.imageList.prefHeightProperty().bind(this.heightProperty().multiply(0.5));
+        this.imageList.prefWidthProperty().bind(this.heightProperty().multiply(0.5/2 * 3).add(200));
     }
 
     public void addLevels(List<String> levels) {
-        for(String level : levels)  {
+        for(String level : levels) {
             this.addLevel(level);
         }
     }
 
     public void addLevel(String level) {
-        int i = this.levelMenu.getRowCount();
+        String path = "thumb/" + level.toLowerCase().replace(" ", "_") + ".png";
+        int i = this.imageList.getLength();
 
-        this.levelMenu.addButton(level, () -> {
-            Game.getInstance().startFromLevel(i);
-        });
+        this.imageList.addImage(
+            level,
+            getClass().getResource(path).toString(),
+            () -> Game.getInstance().startFromLevel(i)
+        );
     }
 }
