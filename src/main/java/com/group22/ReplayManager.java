1c000e78de9240c01c916add3345b607e42c5572
GowerHoldings <78835505+GowerHoldings@users.noreply.github.com>
2022-12-05 22:04:51 +0000

    renamed some variables

--- src/main/java/com/group22/ReplayManager.java
+++ src/main/java/com/group22/ReplayManager.java
@@ -25,16 +25,16 @@ public class ReplayManager {
             String timeForFile = LocalDateTime.now().toString().replace(":", ",");
             String fileName = levelNameForFile + "_" + Game.getInstance().getUsername() + "_" + timeForFile + ".txt" ;
             File replayFile = new File(replayFolder + fileName);
-            FileWriter myWriter = new FileWriter(replayFile, true);
-            myWriter.append(levelNameForFile);
-            myWriter.append(Game.getInstance().getUsername());
-            myWriter.append(LocalDateTime.now().toString() + "\n");
+            FileWriter replayWriter = new FileWriter(replayFile, true);
+            replayWriter.append(level.getTitle() + "\n");
+            replayWriter.append(Game.getInstance().getUsername() + "\n");
+            replayWriter.append(LocalDateTime.now().toString() + "\n");
             for(ReplayFrame frame : this.frames){
-                myWriter.append(frame.getKey() + " " + frame.getKeyTime() + " " + frame.getKeyDown() + "\n");
+                replayWriter.append(frame.getKey() + " " + frame.getKeyTime() + " " + frame.getKeyDown() + "\n");
             }
-            myWriter.close();
+            replayWriter.close();
         } catch (IOException e) {
-            System.out.println("An error occurred writing to the replays.txt file");
+            System.out.println("An error occurred writing to the replays file");
             e.printStackTrace();
         }
     }
