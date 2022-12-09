c3e000aba3cbf5197ca21be46e5d1c4f18fd4e62
lmeekings <117082417+lmeekings@users.noreply.github.com>
2022-12-09 13:20:02 +0000

    module requires jfx media

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.group22 {
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
+    requires javafx.media;
 
     opens com.group22 to javafx.fxml;
     exports com.group22;
