6bb3eaf10e97f24a526c943a2dc3977974ed886d
cleggacus <cleggacus@gmail.com>
2022-12-09 14:41:25 +0000

    Line length reduced

--- src/main/java/com/group22/TimeUtil.java
+++ src/main/java/com/group22/TimeUtil.java
@@ -8,42 +8,56 @@ import java.util.concurrent.TimeUnit;
 
 public class TimeUtil {
 
-    public TimeUtil(){
-
-    }
+    public TimeUtil(){}
 
     public String getTimeAgo(LocalDateTime pastTime) {
-        Period dateBetween = Period.between(pastTime.toLocalDate(), LocalDateTime.now().toLocalDate());
-        Duration timeBetween = Duration.between(pastTime.toLocalTime(), LocalDateTime.now());
+        Period dateBetween = Period.between(
+            pastTime.toLocalDate(), LocalDateTime.now().toLocalDate());
+
+        Duration timeBetween = Duration.between(
+            pastTime.toLocalTime(), LocalDateTime.now());
 
         if (dateBetween.getYears() != 0) {
-            return String.valueOf(dateBetween.getYears())+ 
-                    (dateBetween.getYears() == 1 ? " year" : " years") + " ago";
-        } else if (dateBetween.getMonths() != 0) {
-            return String.valueOf(dateBetween.getMonths())+ 
-                    (dateBetween.getMonths() == 1 ? " month" : " months") + " ago";
-        } else if (dateBetween.getDays() != 0) {
-            return String.valueOf(dateBetween.getDays())+ 
-                    (dateBetween.getDays() == 1 ? " day" : " days") + " ago";
-        } else if (timeBetween.toHours() != 0) {
-            return String.valueOf(timeBetween.toHours())+ 
-                    (timeBetween.toHours() == 1 ? " hour" : " hours") + " ago";
-        } else if (timeBetween.toMinutes() != 0) {
-            return String.valueOf(timeBetween.toMinutes())+ 
-                    (timeBetween.toMinutes() == 1 ? " minute" : " minutes") + " ago";
-        } else {
-            return "A few moments ago";
+            return String.valueOf(dateBetween.getYears()) + 
+                (dateBetween.getYears() == 1 ? " year" : " years") + " ago";
         }
+        
+        if (dateBetween.getMonths() != 0) {
+            return String.valueOf(dateBetween.getMonths()) + 
+                (dateBetween.getMonths() == 1 ? " month" : " months") + " ago";
+        } 
+        
+        if (dateBetween.getDays() != 0) {
+            return String.valueOf(dateBetween.getDays()) + 
+                (dateBetween.getDays() == 1 ? " day" : " days") + " ago";
+        } 
+        
+        if (timeBetween.toHours() != 0) {
+            return String.valueOf(timeBetween.toHours()) + 
+                (timeBetween.toHours() == 1 ? " hour" : " hours") + " ago";
+        } 
+        
+        if (timeBetween.toMinutes() != 0) {
+            return String.valueOf(timeBetween.toMinutes()) + 
+                (timeBetween.toMinutes() == 1 ? " minute" : " minutes") + 
+                " ago";
+        }
+
+        return "A few moments ago";
     }
 
     public String getStringifiedTime(int seconds){
-        if (seconds >= 60){
+        if (seconds >= 60) {
             long minute = TimeUnit.SECONDS.toMinutes(seconds);
             int secMinusMin = (int) (seconds - (60*minute));
-            return minute + (minute == 1 ? " minute" : "  minutes") + secMinusMin + (secMinusMin == 1 ? " second" : " seconds");  
-        } else {
-            return seconds + (seconds == 1 ? " second" : " seconds");
+
+            return minute + 
+                (minute == 1 ? " minute" : "  minutes") + 
+                secMinusMin + 
+                (secMinusMin == 1 ? " second" : " seconds");  
         }
+
+        return seconds + (seconds == 1 ? " second" : " seconds");
     }
 
     public String getLevelTimeLeft(double seconds){
