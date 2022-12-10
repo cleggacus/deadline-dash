23f2a10689538de96599fbea9296a2f983c881d7
cleggacus <cleggacus@gmail.com>
2022-12-10 01:43:56 +0000

    bomb number + vibration

--- src/main/java/com/group22/Bomb.java
+++ src/main/java/com/group22/Bomb.java
@@ -7,21 +7,43 @@ import java.util.ArrayList;
 public class Bomb extends Entity {
 
     private static final double ANIMATION_DURATION = 0.032;
-    private final int countdown;
+    private static final int COUNTDOWN = 3;
+    private Countdown countdownEntity;
     private double time = 0;
-    private double bombExpl;
     private boolean fuze;
     private boolean explosion;
 
+    public class Countdown extends Entity {
+        public Countdown(Bomb bomb) {
+            super(bomb.getX(), bomb.getY());
+
+            this.getSprite().addImageSet("tick", new String[] {
+                "item/farron/countdown3.png",
+                "item/farron/countdown2.png",
+                "item/farron/countdown1.png"
+            });
+        }
+
+        public void activateCountdown() {
+            this.getSprite().setAnimationSpeed(COUNTDOWN);
+            this.getSprite().setImageSet("tick");
+            this.getSprite().setAnimationType(AnimationType.SINGLE);
+        }
+
+        @Override
+        protected void updateMovement() {}
+
+        @Override
+        protected void update() {} 
+    }
 
     /**
      * @param x         the horizontal position of a bomb on the map
      * @param y         the vertical position of a bomb on the map
-     * @param countdown the countdown timer of the bomb's detonation
      */
     public Bomb(int x, int y) {
         super(x, y);
-        this.countdown = 3;
+        this.countdownEntity = new Countdown(this);
         this.fuze = false;
         this.explosion = false;
         this.getSprite().addImageSet("tick", Sprite.createImageFade(
@@ -29,29 +51,33 @@ public class Bomb extends Entity {
                 "item/farron/farron5.png",
                 12 * 3
         ));
+
         this.getSprite().setImage("item/farron/farron0.png");
     }
 
     public void detonateBomb() {
-        ArrayList<Entity> allEntity = new ArrayList();
+        ArrayList<Entity> allEntity = new ArrayList<>();
+
         ArrayList<Bomb> bombs = Game.getInstance().getEntities(Bomb.class);
-        ArrayList<LandMover> landMovers = Game.getInstance().getEntities(LandMover.class);
-        ArrayList<FlyingAssassin> flyingAssassins = Game.getInstance().getEntities(FlyingAssassin.class);
-        ArrayList<PickUp> pickUps = Game.getInstance().getEntities(PickUp.class);
+        ArrayList<LandMover> landMovers = 
+            Game.getInstance().getEntities(LandMover.class);
+        ArrayList<FlyingAssassin> flyingAssassins = 
+            Game.getInstance().getEntities(FlyingAssassin.class);
+        ArrayList<PickUp> pickUps = 
+            Game.getInstance().getEntities(PickUp.class);
+
         allEntity.addAll(bombs);
         allEntity.addAll(landMovers);
         allEntity.addAll(flyingAssassins);
         allEntity.addAll(pickUps);
+
         Game.getInstance().removeEntity(this);
+
         for (Entity entity : allEntity) {
-            if (entity.getX() == this.getX()) {
-                if (entity instanceof Bomb) {
-                    Bomb bomb = (Bomb) entity;
-                    bomb.detonateBomb();
-                } else {
-                    Game.getInstance().removeEntity(entity);
-                }
-            } else if (entity.getY() == this.getY()) {
+            if (
+                entity.getX() == this.getX() ||
+                entity.getY() == this.getY()
+            ) {
                 if (entity instanceof Bomb) {
                     Bomb bomb = (Bomb) entity;
                     bomb.explosion = true;
@@ -64,51 +90,52 @@ public class Bomb extends Entity {
     }
 
     public void activateBomb() {
-        double shakeAmountX = 0.05 * Math.sin(1.1 * countdown * Math.pow(this.time, 3));
-        double shakeAmountY = 0.05 * Math.sin(0.9 * countdown * Math.pow(this.time, 3));
+        this.countdownEntity.activateCountdown();
+
+        double shakeAmountX = 0.05 * 
+            Math.sin(1.1 * COUNTDOWN * Math.pow(this.time, 3));
+
+        double shakeAmountY = 0.05 * 
+            Math.sin(0.9 * COUNTDOWN * Math.pow(this.time, 3));
+
         this.setSpriteOffset(shakeAmountX, shakeAmountY);
         this.time += Game.getInstance().getDelta();
-        this.getSprite().setAnimationSpeed(countdown);
+
+        this.getSprite().setAnimationSpeed(COUNTDOWN);
         this.getSprite().setImageSet("tick");
         this.getSprite().setAnimationType(AnimationType.SINGLE);
 
         ArrayList<Bomb> bombs = Game.getInstance().getEntities(Bomb.class);
-        if (this.time >= countdown - ANIMATION_DURATION) {
+        if (this.time >= COUNTDOWN - ANIMATION_DURATION) {
             for (Bomb bomb : bombs) {
-                if (bomb.getX() == this.getX()) {
-                    bomb.explosion = true;
-                } else if (bomb.getY() == this.getY()) {
+                if (
+                    bomb.getX() == this.getX() || 
+                    bomb.getY() == this.getY()
+                ) {
                     bomb.explosion = true;
                 }
             }
         }
 
 
-        if (this.time >= countdown) {
+        if (this.time >= COUNTDOWN) {
             detonateBomb();
         }
     }
 
     @Override
-    protected void updateMovement() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
     public void draw(Renderer renderer) {
         super.draw(renderer);
+        this.countdownEntity.draw(renderer);
+
         if (this.explosion) {
             for (int x = 0; x < renderer.getViewWidth(); x++) {
                 renderer.drawRect(x, (this.getY() - 0.05), 1, 0.05, Color.RED);
-            }
-            for (int x = 0; x < renderer.getViewWidth(); x++) {
                 renderer.drawRect(x, (this.getY() + 0.05), 1, 0.05, Color.RED);
             }
+
             for (int y = 0; y < renderer.getViewHeight(); y++) {
                 renderer.drawRect((this.getX() - 0.05), y, 0.05, 1, Color.RED);
-            }
-            for (int y = 0; y < renderer.getViewHeight(); y++) {
                 renderer.drawRect((this.getX() + 0.05), y, 0.05, 1, Color.RED);
             }
         }
@@ -116,31 +143,29 @@ public class Bomb extends Entity {
 
     @Override
     protected void update() {
-        ArrayList<LandMover> landMovers = Game.getInstance().getEntities(LandMover.class);
+        this.countdownEntity.callUpdate();
+
+        ArrayList<LandMover> landMovers = 
+            Game.getInstance().getEntities(LandMover.class);
+
         if (!fuze) {
             for (LandMover landMover : landMovers) {
-                if (landMover.getX() == this.getX() && landMover.getY() == (this.getY() - 1)) {
+                int displacementX = Math.abs(landMover.getX() - this.getX());
+                int displacementY = Math.abs(landMover.getY() - this.getY());
+                int displacement = displacementX + displacementY;
 
-                    this.fuze = true;
-                } else if (landMover.getX() == this.getX() && landMover.getY() == (this.getY() + 1)) {
-
-                    this.fuze = true;
-                } else if (landMover.getX() == (this.getX() - 1) && landMover.getY() == (this.getY())) {
-
-                    this.fuze = true;
-                } else if (landMover.getX() == (this.getX() + 1) && landMover.getY() == (this.getY())) {
-
+                if (displacement == 1) {
                     this.fuze = true;
                 }
             }
-        }
-        if (fuze) {
+        } else {
             activateBomb();
         }
 
-        if (this.time >= countdown - ANIMATION_DURATION){
-            explosion = true;
-        }
+        this.explosion = this.time >= COUNTDOWN - ANIMATION_DURATION;
 
     }
+
+    @Override
+    protected void updateMovement() {}
 }
