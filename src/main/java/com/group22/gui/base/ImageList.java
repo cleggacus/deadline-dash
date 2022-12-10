03d0d248f1ea0039c2067f53166e99a3b1c6f88a
cleggacus <cleggacus@gmail.com>
2022-12-10 18:11:44 +0000

    Replay GUI

--- src/main/java/com/group22/gui/base/ImageList.java
+++ src/main/java/com/group22/gui/base/ImageList.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 
 import com.group22.gui.base.ListButton.OnClickEvent;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 
@@ -45,21 +50,20 @@ public class ImageList extends ScrollPane {
         return stackPanes;
     }
 
-    public void addImage(String text, String path, OnClickEvent onClickEvent, String footerText, OnClickEvent onFooterClickEvent) {
+    public void addImage(
+        String text, 
+        String path, 
+        OnClickEvent onClickEvent, 
+        Button[] footerButtons
+    ) {
         BorderPane outer = new BorderPane();
 
         StackPane hoverImage = createHoverImage(text, path);
-        BorderPane footer = createFooter(footerText);
+        GridPane footer = createFooter(footerButtons);
 
         hoverImage.setOnMouseClicked(e -> {
-            if(e.getButton() == MouseButton.PRIMARY) {
+            if (e.getButton() == MouseButton.PRIMARY) {
                 onClickEvent.run();
-            }
-        });
-
-        footer.setOnMouseClicked(e -> {
-            if(e.getButton() == MouseButton.PRIMARY) {
-                onFooterClickEvent.run();
             }
         });
 
@@ -71,33 +75,46 @@ public class ImageList extends ScrollPane {
         this.grid.add(outer, this.grid.getColumnCount(), 0);
     }
 
-    private BorderPane createFooter(String text) {
-        BorderPane borderPane = new BorderPane();
-        Label label = new Label(text);
-        
-        borderPane.getStyleClass().add("footer");
+    private GridPane createFooter(Button[] buttons) {
+        GridPane gridPane = new GridPane();
 
-        borderPane.setCenter(label);
+        gridPane.getStyleClass().add("footer");
 
-        return borderPane;
+        ColumnConstraints column = new ColumnConstraints();
+        column.setHalignment(HPos.CENTER);
+        column.setPercentWidth(100 / buttons.length);
+
+        for (int i = 0; i < buttons.length; i++) {
+            Button button = buttons[i];
+            gridPane.addColumn(i, button);
+            gridPane.getColumnConstraints().add(column);
+        }
+
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        gridPane.setAlignment(Pos.CENTER);
+
+        return gridPane;
     }
 
     private StackPane createHoverImage(String text, String path) {
         StackPane stackPane = new StackPane();
         stackPane.setMinHeight(0);
 
-
         ImageView imageView = new ImageView(path);
         imageView.setPreserveRatio(true);
         imageView.fitHeightProperty().bind(stackPane.heightProperty());
-
 
         BorderPane borderPane = new BorderPane();
         Label label = new Label(text);
         label.setWrapText(true);
 
-        double ratio = imageView.getImage().getWidth() / imageView.getImage().getHeight();
-        label.prefWidthProperty().bind(imageView.fitHeightProperty().multiply(ratio));
+        double ratio = imageView.getImage().getWidth() / 
+            imageView.getImage().getHeight();
+
+        label.prefWidthProperty().bind(
+            imageView.fitHeightProperty().multiply(ratio));
 
         borderPane.setCenter(label);
         borderPane.getStyleClass().add("text-overlay");
@@ -110,7 +127,7 @@ public class ImageList extends ScrollPane {
         int i = this.stackPanes.size();
 
         stackPane.setOnMouseEntered(e -> {
-            if(unhoverImage == i) {
+            if (unhoverImage == i) {
                 unhoverImage = -1;
                 hoverTimer = duration - unhoverTimer;
                 unhoverTimer = 0;
@@ -120,7 +137,7 @@ public class ImageList extends ScrollPane {
         });
 
         stackPane.setOnMouseExited(e -> {
-            if(hoverImage == i) {
+            if (hoverImage == i) {
                 hoverImage = -1;
                 unhoverTimer = duration - hoverTimer;
                 hoverTimer = 0;
@@ -144,7 +161,7 @@ public class ImageList extends ScrollPane {
 
     public void update(double delta) {
 
-        if(this.hoverImage >= 0) {
+        if (this.hoverImage >= 0) {
             double amount = this.hoverTimer / duration;
             amount = amount > 1 ? 1 : amount;
 
@@ -153,11 +170,12 @@ public class ImageList extends ScrollPane {
             BorderPane overlay = (BorderPane)stackPane.getChildren().get(1);
             GaussianBlur gaussianBlur = new GaussianBlur(amount * 10);
 
-            imageView.setClip(new Rectangle(stackPane.getWidth(), stackPane.getHeight()));
+            imageView.setClip(new Rectangle(
+                stackPane.getWidth(), stackPane.getHeight()));
             imageView.setEffect(gaussianBlur);
             overlay.setOpacity(amount);
 
-            if(this.hoverTimer > duration) {
+            if (this.hoverTimer > duration) {
                 this.hoverImage = -1;
                 this.hoverTimer = 0;
             } else {
@@ -165,7 +183,7 @@ public class ImageList extends ScrollPane {
             }
         }
 
-        if(this.unhoverImage >= 0) {
+        if (this.unhoverImage >= 0) {
             double amount = 1 - (this.unhoverTimer / duration);
             amount = amount < 0 ? 0 : amount;
 
@@ -175,11 +193,12 @@ public class ImageList extends ScrollPane {
 
             GaussianBlur gaussianBlur = new GaussianBlur(amount * 10);
 
-            imageView.setClip(new Rectangle(stackPane.getWidth(), stackPane.getHeight()));
+            imageView.setClip(new Rectangle(
+                stackPane.getWidth(), stackPane.getHeight()));
             imageView.setEffect(gaussianBlur);
             overlay.setOpacity(amount);
 
-            if(this.unhoverTimer > duration) {
+            if (this.unhoverTimer > duration) {
                 this.unhoverImage = -1;
                 this.unhoverTimer = 0;
             } else {
