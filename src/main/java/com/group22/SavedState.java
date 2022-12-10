ca3d39f2c7ab053551d49cd493b7dce77b875d02
RhysMcguire <rhys.mcguire@hotmail.com>
2022-12-10 12:40:55 +0000

    checkstyle pt3.

--- src/main/java/com/group22/SavedState.java
+++ src/main/java/com/group22/SavedState.java
@@ -3,6 +3,9 @@ package com.group22;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+/**
+ * 
+ */
 public class SavedState {
     private String levelTitle;
     private ArrayList<Entity> entities;
@@ -11,7 +14,17 @@ public class SavedState {
     private double time;
     private int levelIndex;
 
-    public SavedState(String levelTitle, ArrayList<Entity> entities, int score, double time, LocalDateTime timeOfSave, int levelIndex){
+    /**
+     * 
+     * @param levelTitle
+     * @param entities
+     * @param score
+     * @param time
+     * @param timeOfSave
+     * @param levelIndex
+     */
+    public SavedState(String levelTitle, ArrayList<Entity> entities, 
+        int score, double time, LocalDateTime timeOfSave, int levelIndex) {
         this.levelTitle = levelTitle;
         this.entities = entities;
         this.score = score;
@@ -20,29 +33,51 @@ public class SavedState {
         this.levelIndex = levelIndex;
     }
 
-    public LocalDateTime getTimeOfSave(){
+    
+    /** 
+     * @return LocalDateTime
+     */
+    public LocalDateTime getTimeOfSave() {
         return timeOfSave;
     }
 
-    public String getLevelTitle(){
+    
+    /** 
+     * @return String
+     */
+    public String getLevelTitle() {
         return levelTitle;
     }
 
-    public int getLevelIndex(){
+    
+    /** 
+     * @return int
+     */
+    public int getLevelIndex() {
         return levelIndex;
     }
 
-    public ArrayList<Entity> getEntities(){
+    
+    /** 
+     * @return {@link ArrayList}{@link Entity}
+     */
+    public ArrayList<Entity> getEntities() {
         return entities;
     }
 
-    public int getScore(){
+    
+    /** 
+     * @return int
+     */
+    public int getScore() {
         return score;
     }
 
-    public double getTime(){
+    
+    /** 
+     * @return double
+     */
+    public double getTime() {
         return time;
     }
-
-
 }
--- src/main/java/com/group22/SavedStateManager.java
+++ src/main/java/com/group22/SavedStateManager.java
@@ -4,14 +4,28 @@ import java.io.File;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+/**
+ * 
+ */
 public class SavedStateManager {
     private String statesFolder = "src/main/resources/com/group22/states/";
     private FileManager fileManager;
-    public SavedStateManager(){
+
+    public SavedStateManager() {
         fileManager = new FileManager();
     }
 
-    public void createState(String levelTitle, String username, ArrayList<Entity> entities, int score, double time, int levelIndex){
+    
+    /** 
+     * @param levelTitle
+     * @param username
+     * @param entities
+     * @param score
+     * @param time
+     * @param levelIndex
+     */
+    public void createState(String levelTitle, String username, 
+        ArrayList<Entity> entities, int score, double time, int levelIndex) {
         ArrayList<String> data = new ArrayList<String>();
 
         data.add(levelTitle);
@@ -20,23 +34,31 @@ public class SavedStateManager {
         data.add(String.valueOf(time));
         data.add(String.valueOf(levelIndex));
 
-        for(Entity entity : entities){
-            if(!(entity instanceof Tile)){
+        for (Entity entity : entities) {
+            if (!(entity instanceof Tile)) {
                 String entityString = entity.toString();
                 data.add(entityString);
             }
         }
 
         String fileName = levelTitle.replace(" ", "-") + "_" + username + "_" +
-        LocalDateTime.now().toString().replace(":", ",") + ".txt";
+            LocalDateTime.now().toString().replace(":", ",") + ".txt";
         File file = new File(statesFolder + fileName);
 
         fileManager.saveFile(data, file);
     }
+    
+    /** 
+     * @param level
+     * @param username
+     * @return {@link ArrayList}{@link SavedState}
+     */
     public ArrayList<SavedState> getStates(Level level, String username){
         String startsWith = level.getTitle().replace(" ", "-") + "_" + username;
-        File[] files = fileManager.getMatchingFiles(statesFolder, startsWith, ".txt");
+        File[] files = 
+            fileManager.getMatchingFiles(statesFolder, startsWith, ".txt");
         ArrayList<SavedState> savedStates = new ArrayList<SavedState>();
+
         for (File file : files) {
             ArrayList<String> data = fileManager.getDataFromFile(file);
             int score = Integer.parseInt(data.get(2));
@@ -44,6 +66,7 @@ public class SavedStateManager {
             
             ArrayList<Entity> entities = new ArrayList<Entity>();
             entities.addAll(level.getTilesAsEntities());
+            
             for (String entityString : data.subList(5, data.size())) {
                 try {
                     entities.add(level.parseEntity(entityString.split(" ")));
@@ -52,9 +75,11 @@ public class SavedStateManager {
                 }
             }
 
-            SavedState savedState = new SavedState(level.getTitle(), entities, score, time, LocalDateTime.now(), level.getIndex());
+            SavedState savedState = new SavedState(level.getTitle(), 
+                entities, score, time, LocalDateTime.now(), level.getIndex());
             savedStates.add(savedState);
         }
+
         return savedStates;
     }
 
