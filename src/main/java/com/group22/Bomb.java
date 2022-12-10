304d47a5ac993d172ecff7461f0775db48a88519
cleggacus <cleggacus@gmail.com>
2022-12-10 03:39:25 +0000

    bomb lazers

--- src/main/java/com/group22/Bomb.java
+++ src/main/java/com/group22/Bomb.java
@@ -1,13 +1,13 @@
 package com.group22;
 
-import javafx.scene.paint.Color;
-
 import java.util.ArrayList;
+
 
 public class Bomb extends Entity {
 
-    private static final double ANIMATION_DURATION = 0.032;
+    private static final double ANIMATION_DURATION = 0.2;
     private static final int COUNTDOWN = 3;
+    private ArrayList<Lazer> lazers = new ArrayList<>();
     private Countdown countdownEntity;
     private double time = 0;
     private boolean fuze;
@@ -37,6 +37,101 @@ public class Bomb extends Entity {
         protected void update() {} 
     }
 
+    public class Lazer extends Entity {
+        private double time = 0;
+        private Bomb bomb;
+        private boolean isVertical;
+        private boolean isHorizantal;
+        private boolean isActive;
+
+        public Lazer(int x, int y, Bomb bomb) {
+            super(x, y);
+
+            this.bomb = bomb;
+
+            this.isHorizantal = y == bomb.getY();
+            this.isVertical = x == bomb.getX();
+            boolean isCenter = this.isVertical && this.isHorizantal;
+
+            String suffix = 
+                isCenter ? "center" :
+                isHorizantal ? "h" : "v";
+
+            this.getSprite().setImage(
+                "item/farron/lazer_" + suffix + ".png");
+        }
+
+        @Override
+        public void draw(Renderer renderer) {
+            double animationTime = time / ANIMATION_DURATION;
+
+            int bombDisplacementX = Math.abs(this.getX() - this.bomb.getX());
+            int bombDisplacementY = Math.abs(this.getY() - this.bomb.getY());
+            int bombDisplacement = bombDisplacementX + bombDisplacementY;
+
+            int bombEdgeDistanceX = Math.max(
+                renderer.getViewWidth() - this.bomb.getX(), this.bomb.getX());
+
+            int bombEdgeDistanceY = Math.max(
+                renderer.getViewHeight() - this.bomb.getY(), this.bomb.getY());
+
+            int bombEdgeDistance = Math.max(
+                bombEdgeDistanceX, bombEdgeDistanceY);
+
+            double visibleFrame = 
+                (double)bombDisplacement / (double)bombEdgeDistance;
+
+            if (animationTime >= visibleFrame) {
+                super.draw(renderer);
+            }
+        }
+
+        @Override
+        protected void updateMovement() {}
+
+        @Override
+        protected void update() {
+            this.time += Game.getInstance().getDelta();
+            
+            this.updateShake();
+            this.updateIsActive();
+        }
+
+        private void updateShake() {
+            double shakeAmount = 0.1*Math.sin(
+                this.time * Math.PI * 2 * 
+                (1/ANIMATION_DURATION) * 5);
+
+            this.setSpriteOffset(
+                isVertical ? shakeAmount : 0, 
+                isHorizantal ? shakeAmount : 0);
+        }
+
+        private void updateIsActive() {
+            Renderer renderer = Game.getInstance().getRenderer();
+            
+            double animationTime = time / ANIMATION_DURATION;
+
+            int bombDisplacementX = Math.abs(this.getX() - this.bomb.getX());
+            int bombDisplacementY = Math.abs(this.getY() - this.bomb.getY());
+            int bombDisplacement = bombDisplacementX + bombDisplacementY;
+
+            int bombEdgeDistanceX = Math.max(
+                renderer.getViewWidth() - this.bomb.getX(), this.bomb.getX());
+
+            int bombEdgeDistanceY = Math.max(
+                renderer.getViewHeight() - this.bomb.getY(), this.bomb.getY());
+
+            int bombEdgeDistance = Math.max(
+                bombEdgeDistanceX, bombEdgeDistanceY);
+
+            double visibleFrame = 
+                (double)bombDisplacement / (double)bombEdgeDistance;
+
+            this.isActive = animationTime >= visibleFrame;
+        }
+    }
+
     /**
      * @param x         the horizontal position of a bomb on the map
      * @param y         the vertical position of a bomb on the map
@@ -47,12 +142,16 @@ public class Bomb extends Entity {
         this.fuze = false;
         this.explosion = false;
         this.getSprite().addImageSet("tick", Sprite.createImageFade(
-                "item/farron/farron0.png",
-                "item/farron/farron5.png",
-                12 * 3
+            "item/farron/farron0.png",
+            "item/farron/farron5.png",
+            12 * 3
         ));
 
         this.getSprite().setImage("item/farron/farron0.png");
+    }
+
+    public double getTime() {
+        return time;
     }
 
     public void detonateBomb() {
@@ -126,23 +225,36 @@ public class Bomb extends Entity {
     @Override
     public void draw(Renderer renderer) {
         super.draw(renderer);
+
         this.countdownEntity.draw(renderer);
 
-        if (this.explosion) {
-            for (int x = 0; x < renderer.getViewWidth(); x++) {
-                renderer.drawRect(x, (this.getY() - 0.05), 1, 0.05, Color.RED);
-                renderer.drawRect(x, (this.getY() + 0.05), 1, 0.05, Color.RED);
-            }
-
-            for (int y = 0; y < renderer.getViewHeight(); y++) {
-                renderer.drawRect((this.getX() - 0.05), y, 0.05, 1, Color.RED);
-                renderer.drawRect((this.getX() + 0.05), y, 0.05, 1, Color.RED);
-            }
+        if (this.explosion && this.lazers.isEmpty()) {
+            createLazers();
         }
+
+        for (Lazer lazer : lazers) {
+            lazer.draw(renderer);
+        }
+
+        // if (this.explosion) {
+        //     for (int x = 0; x < renderer.getViewWidth(); x++) {
+        //         renderer.drawRect(x, (this.getY() - 0.05), 1, 0.05, Color.RED);
+        //         renderer.drawRect(x, (this.getY() + 0.05), 1, 0.05, Color.RED);
+        //     }
+
+        //     for (int y = 0; y < renderer.getViewHeight(); y++) {
+        //         renderer.drawRect((this.getX() - 0.05), y, 0.05, 1, Color.RED);
+        //         renderer.drawRect((this.getX() + 0.05), y, 0.05, 1, Color.RED);
+        //     }
+        // }
     }
 
     @Override
     protected void update() {
+        for (Lazer lazer : lazers) {
+            lazer.callUpdate();
+        }
+
         this.countdownEntity.callUpdate();
 
         ArrayList<LandMover> landMovers = 
@@ -162,11 +274,33 @@ public class Bomb extends Entity {
             activateBomb();
         }
 
-        if(this.time >= COUNTDOWN - ANIMATION_DURATION) {
+        if (this.time >= COUNTDOWN - ANIMATION_DURATION) {
             this.explosion = true;
         }
     }
 
     @Override
     protected void updateMovement() {}
+
+    private void createLazers() {
+        int width = Game.getInstance().getRenderer().getViewWidth();
+        int height = Game.getInstance().getRenderer().getViewHeight();
+
+        for (int x = 0; x < width; x++) {
+            if (x != this.getX()) {
+                this.lazers.add(
+                    new Lazer(x, this.getY(), this));
+            }
+        }
+
+        for (int y = 0; y < height; y++) {
+            if (y != this.getY()) {
+                this.lazers.add(
+                    new Lazer(this.getX(), y, this));
+            }
+        }
+
+        this.lazers.add(
+            new Lazer(this.getX(), this.getY(), this));
+    }
 }
