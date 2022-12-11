d4638008043763e1c2b4058a52e71f779e57b557
cleggacus <cleggacus@gmail.com>
2022-12-11 02:13:14 +0000

    Input style check

--- src/main/java/com/group22/gui/base/Input.java
+++ src/main/java/com/group22/gui/base/Input.java
@@ -5,16 +5,33 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 
+/**
+ * 
+ * The class {@code Input} is a text field with a submit button which extends 
+ * BorderPane. 
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
 public class Input extends BorderPane {
+
+    /**
+     * Creates an Input GUI element with placeholder, button text and an on 
+     * enter event.
+     * 
+     * @param placeholder Prompt text for input field.
+     * @param buttonText Text in submit button in input.
+     * @param onEnter Called when button is clicked or the enter key is pressed.
+     */
     public Input(String placeholder, String buttonText, InputEvent onEnter) {
         Button button = new Button(buttonText);
 
-        TextField textField= new TextField();
+        TextField textField = new TextField();
 
         textField.setPromptText(placeholder);
 
         textField.setOnKeyPressed(e -> {
-            if(e.getCode() == KeyCode.ENTER) {
+            if (e.getCode() == KeyCode.ENTER) {
                 onEnter.run(textField.getText());
             }
         });
@@ -27,6 +44,11 @@ public class Input extends BorderPane {
         this.getStyleClass().add("input");
     }
 
+    /**
+     * InputEvent intereface which calls run when input is entered.
+     * Can be used by class that impliments {@code InputEvent} or 
+     * a lambda function.
+     */
     public interface InputEvent {
         void run(String s);
     }
