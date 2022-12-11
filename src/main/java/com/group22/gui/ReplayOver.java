7692adf4f642562e3aeae6f6d0edc2fbdf57b8b2
cleggacus <cleggacus@gmail.com>
2022-12-11 04:05:28 +0000

    Replay over javadocs

--- src/main/java/com/group22/gui/ReplayOver.java
+++ src/main/java/com/group22/gui/ReplayOver.java
@@ -7,10 +7,22 @@ import com.group22.gui.base.MenuPane;
 
 import javafx.scene.control.Label;
 
+/**
+ * The class {@code ReplayOver} is the pane which is shown when a 
+ * replay has ended.
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
 public class ReplayOver extends MenuPane {
+    /** Label which contains score text. */
     private Label scoreLabel;
+    /** Label which contains time text. */
     private Label timeLabel;
 
+    /**
+     * Creates ReplayOver instance.
+     */
     public ReplayOver() {
         this.addH1("REPLAY COMPLETE");
         this.addButton("EXIT", 
@@ -21,6 +33,12 @@ public class ReplayOver extends MenuPane {
         this.getStyleClass().add("gameover-menu");
     }
 
+    /**
+     * Sets the score and time gui elements.
+     * 
+     * @param score Sets {@link #scoreLabel} text to score value.
+     * @param time Sets {@link #timeLabel} text to score value.
+     */
     public void setStats(int score, double time){
         this.timeLabel.setText("Time: " + TimeUtil.getLevelTimeLeft(time));
         this.scoreLabel.setText("Score: " + String.valueOf(score));
