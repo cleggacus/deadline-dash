9ba064c9b5b1d5035204f9e3750744dd90473e9b
cleggacus <cleggacus@gmail.com>
2022-12-11 03:01:24 +0000

    Credits style

--- src/main/java/com/group22/gui/Credits.java
+++ src/main/java/com/group22/gui/Credits.java
@@ -4,7 +4,18 @@ import com.group22.Game;
 import com.group22.GameState;
 import com.group22.gui.base.MenuPane;
 
+/**
+ * The class {@code Credits} extends MenuPane and shows the credits 
+ * of the game. 
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
 public class Credits extends MenuPane {
+
+    /**
+     * Creates credits.
+     */
     public Credits() {
         this.addH1("CREDITS");
         this.addParagraph("LIAM CLEGG");
@@ -14,6 +25,7 @@ public class Credits extends MenuPane {
         this.addParagraph("LEWIS MEEKINGS");
         this.addParagraph("STEFFAN BORLAND");
         this.addParagraph("EZANA TAREKE");
-        this.addButton("EXIT", () -> Game.getInstance().setGameState(GameState.Start));
+        this.addButton("EXIT", 
+            () -> Game.getInstance().setGameState(GameState.Start));
     }
 }
