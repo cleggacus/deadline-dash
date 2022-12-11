d9ba29b23847235929147fcc033d4c65a85dedd2
cleggacus <cleggacus@gmail.com>
2022-12-11 03:06:47 +0000

    GameOver Style

--- src/main/java/com/group22/gui/GameOver.java
+++ src/main/java/com/group22/gui/GameOver.java
@@ -4,24 +4,44 @@ import com.group22.Game;
 import com.group22.GameState;
 import com.group22.gui.base.MenuPane;
 
-public class GameOver extends MenuPane {
-    private GamePane gamePane;
-    public GameOver(GamePane gamePane) {
-        this.gamePane = gamePane;
+import javafx.scene.control.Label;
 
-        setUpGameLost();
+/**
+ * The class {@code GameOver} is a menu for when someone loses the game 
+ * which extends MenuPane.
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
+public class GameOver extends MenuPane {
+    private Label scoreLabel;
+    private Label timeLabel;
+
+    /**
+     * Creates GameOver pane.
+     */
+    public GameOver() {
+        this.getChildren().clear();
+        this.addH1("GAMEOVER");
+        this.addButton("RESTART", 
+            () -> Game.getInstance().setGameState(GameState.Playing));
+        this.addButton("EXIT", 
+            () -> Game.getInstance().setGameState(GameState.Start));
+
+        this.scoreLabel = this.addParagraph("");
+        this.timeLabel = this.addParagraph("");
 
         this.getStyleClass().add("gameover-menu");
     }
-    private void setUpGameLost(){
-        this.getChildren().clear();
-        this.addH1("GAMEOVER");
-        this.addButton("RESTART", () -> Game.getInstance().setGameState(GameState.Playing));
-        this.addButton("EXIT", () -> gamePane.setState(GameState.Start));
-    }
 
+    /**
+     * Sets the stats in the GameOver pane.
+     * 
+     * @param score integer for the score to be displayed.
+     * @param time double for the time to be displayed.
+     */
     public void setStats(int score, double time) {
-        setUpGameLost();
-        this.addParagraph("Score: " + String.valueOf(score));
+        this.scoreLabel.setText("SCORE: " + score);
+        this.timeLabel.setText("TIME: " + time);
     }
 }
