7367453df437a0d7eb6cd2404571f63e0d7be072
cleggacus <cleggacus@gmail.com>
2022-12-11 04:43:31 +0000

    AnimationType javadocs

--- src/main/java/com/group22/AnimationType.java
+++ src/main/java/com/group22/AnimationType.java
@@ -1,5 +1,11 @@
 package com.group22;
 
+/**
+ * The Enum {@code AnimationType} represents the animation type in a sprite.
+ * 
+ * @author Liam Clegg
+ * @version 1.0
+ */
 public enum AnimationType {
     NONE, INFINITE, SINGLE
 }
